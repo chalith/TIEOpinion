c147a7caf178676896076455a2a27b7136f8133d
chalith <desaman.chalith@gmail.com>
2018-12-30 01:53:15 +0530

    slash

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiPath: "https://tieapi.herokuapp.com/",
+  apiPath: "https://tieapi.herokuapp.com",
   appPath: "https://tieopinion.firebaseapp.com",
   serviceName: "TIEOpinion"
 };
